c3ab2dd379e55d21b73a2038ce1dce969e566c5f
P Panthong <panthari.panthong@gmail.com>
2019-10-16 17:12:38 +0200

    Support creating new data

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json()
 const port = process.env.PORT || 4000
 
 // const db = require('./db')
 // const model = require('./team/model')
 const teamRouter = require('./team/router')
 
-
+app.use(jsonParser)
 app.use(teamRouter)
 app.listen(port, () => console.log(`Listening on :${port}`))
--- team/router.js
+++ team/router.js
@@ -6,9 +6,16 @@ const router = new Router()
 
 router.get('/team', (req, res, next) =>
   Team.findAll()
+    //receive the list of teams. Send the list as the response.
     .then(result => res.json(result))
+    //receive an error if it is thrown. Pass it to next.
     .catch(error => next(error))
 )
 
+router.post('/team', (req, res, next) =>
+  Team.create(req.body)
+    .then(team => res.json(team))
+    .catch(error => next(error))
+)
 
 module.exports = router
